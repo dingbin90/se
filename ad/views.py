32a7c4465acb0ac41a60578bb038e4eac091abb6
dingbin90 <32011793+dingbin90@users.noreply.github.com>
2017-12-25 09:44:56 +0800

    Initial commit diyicu tijiao

---
+++ ad/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render,redirect,HttpResponseRedirect
+
+# Create your views here.
+from ad import models
+
+
+
+def index(request):
+
+    pass
+    return render(request,'index.html')
+
+
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        message = "所有字段都必须填写！"
+        if username and password:
+            username = username.strip()
+            try:
+                user = models.User.objects.get(name = username)
+                print(user.name)
+                print('###########3')
+                print(user.passwd)
+                if user.passwd == password:
+                    return redirect('index/')
+                else:
+                    message = "密码不正确"
+            except:
+               message = "用户不存在"
+        # print(username,password)
+        return render(request, 'login.html', {"message": message})
+    return render(request,'login.html')
+
+
+
+
+
+def register(request):
+    pass
+    return render(request,'register.html')
+
+
+def logout(request):
+    pass
+
